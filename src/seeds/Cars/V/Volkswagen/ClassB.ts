26ff8ca8001b196cb39172b68c8d3ac7c1c9a9f8
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-12 15:32:06 -0600

    finished separting cars into multiple files

---
+++ src/seeds/Cars/V/Volkswagen/ClassB.ts
@@ -0,0 +1,3 @@
+import w12Coupe from '@/seeds/Cars/V/Volkswagen/B/w12Coupe.json';
+
+export default [w12Coupe];
---
+++ src/seeds/Cars/V/Volkswagen/ClassD.ts
@@ -0,0 +1,4 @@
+import xlSportConcept from '@/seeds/Cars/V/Volkswagen/D/xlSportConcept.json';
+import electricR from '@/seeds/Cars/V/Volkswagen/D/electricR.json';
+
+export default [xlSportConcept, electricR];
