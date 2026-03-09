bcc58ee31b149dc732041c9bc35e2c331d033f82
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-03-09 18:38:18 -0500

    updated data for Glickenhaus 007s

---
+++ src/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/index.ts
@@ -0,0 +1,7 @@
+import oneStar from '@/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/1star.json';
+import twoStar from '@/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/2star.json';
+import threeStar from '@/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/3star.json';
+import fourStar from '@/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/4star.json';
+import fiveStar from '@/seeds/cars/G/Glickenhaus/A/007s/deltas/stages/5star.json';
+
+export default { stagesDeltas: { oneStar, twoStar, threeStar, fourStar, fiveStar }};
---
+++ src/seeds/cars/G/Glickenhaus/A/007s/upgrades/index.ts
@@ -0,0 +1,6 @@
+import creditCosts from '@/seeds/cars/G/Glickenhaus/A/007s/upgrades/creditCosts.json';
+import garageLevelXp from '@/seeds/cars/G/Glickenhaus/A/007s/upgrades/garageLevelXp.json';
+import imports from '@/seeds/cars/G/Glickenhaus/A/007s/upgrades/imports';
+import progression from '@/seeds/cars/G/Glickenhaus/A/007s/upgrades/progression.json';
+
+export default { creditCosts, garageLevelXp, ...imports, progression };
