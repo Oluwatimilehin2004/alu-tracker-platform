a93acc6143763f0ca90a5eb629d8ac26faa127bb
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-03-19 10:20:50 -0500

    updated data for several cars

---
+++ src/seeds/cars/I/Infiniti/D/ProjectBlackS/upgrades/imports/index.ts
@@ -0,0 +1,5 @@
+import costs from "@/seeds/cars/I/Infiniti/D/ProjectBlackS/upgrades/imports/costs.json";
+import garageLevelXp from "@/seeds/cars/I/Infiniti/D/ProjectBlackS/upgrades/imports/garageLevelXp.json";
+import requirements from "@/seeds/cars/I/Infiniti/D/ProjectBlackS/upgrades/imports/requirements.json";
+
+export default { imports: { costs, garageLevelXp, requirements } };
